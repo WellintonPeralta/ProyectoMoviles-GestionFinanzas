2dafd036d98ec3450bf1b1aa215c23b52056cb5b
stevenq9 <steven.quispe@epn.edu.ec>
2023-09-14 23:53:57 -0500

    feat: App Gestión de Finanzas personales

--- app/src/main/java/com/example/gestionfinanzas/Actividades/Autenticar.kt
+++ app/src/main/java/com/example/gestionfinanzas/Actividades/Autenticar.kt
@@ -35,6 +35,7 @@ class Autenticar : AppCompatActivity() {
                     } else {
                         Log.e("Error", "No puede logear al usuario")
                     }
+
                 }
             }
 
